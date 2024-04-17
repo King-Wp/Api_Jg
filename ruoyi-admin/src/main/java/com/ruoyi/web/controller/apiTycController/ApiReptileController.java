10d3e50168684253223ea7c166a81ae7ae38359f
LGY <1165370209@qq.com>
2024-04-17 16:25:51 +0800

    注解添加

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/apiTycController/ApiReptileController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/apiTycController/ApiReptileController.java
@@ -32,7 +32,7 @@ public class ApiReptileController {
      * contracts 单位名称查询业绩合同名称
      */
     @PostMapping("/customer/keyWords")
-    public String[] keyToLevel(@RequestBody KeyWordsParams keyWordsParams){
+    public String[] keyToLevel(@RequestBody KeyWordsParams keyWordsParams) {
         return reptileService.keyToLevel(keyWordsParams);
     }
 
@@ -43,53 +43,61 @@ public class ApiReptileController {
      * bidTitles 单位名称查询招投标标题金额top10
      * contracts 单位名称查询业绩合同名称
      * info
+     *
      * @param keyWordsParams 参数对象
      */
     @PostMapping("/customer/visitKeyWord")
-    public String[] visitKeyWord(@RequestBody KeyWordsParams keyWordsParams){
+    public String[] visitKeyWord(@RequestBody KeyWordsParams keyWordsParams) {
         return reptileService.getVisitKeyWord(keyWordsParams);
     }
 
     /**
      * RtbReportP 商机报备列表
      * baseId 商机id
+     *
      * @param keyWordsParams 参数对象
      */
     @PostMapping("/customer/reportKeyWord")
-    public JSONObject reportKeyWord(@RequestBody KeyWordsParams keyWordsParams){
+    public JSONObject reportKeyWord(@RequestBody KeyWordsParams keyWordsParams) {
         return reptileService.getReportKeyword(keyWordsParams);
     }
 
     /**
      * 免费获取工商信息
+     *
      * @param companyName 企业名称
-     * @param userName 创建用户
+     * @param userName    创建用户
      */
     @GetMapping("/addBusinessFreeByTyc")
-    public void addBusinessFreeByTyc(String companyName, String userName){
+    public void addBusinessFreeByTyc(@RequestParam("companyName")String companyName, @RequestParam("userName")String userName) {
         reptileService.addBusinessFreeByTyc(companyName, userName);
     }
 
     /**
      * 获取公司简介
+     *
      * @param companyId 企业id
      * @return 简介
      */
     @GetMapping("/introduction")
-    public String getCompanyProfile(String companyId){
+    public String getCompanyProfile(@RequestParam("companyId")String companyId) {
         return reptileService.getCompanyProfile(companyId);
     }
 
     /**
-     *  客户单位的工商信息
-     * @param companyId 企业id
-     * @param userName 用户名称
-     * @param queryKeyword 查询关键字
+     * 客户单位的工商信息
+     *
+     * @param companyId      企业id
+     * @param userName       用户名称
+     * @param queryKeyword   查询关键字
      * @param enterpriseType 获取祥云中的企业性质
      * @return 入库结果
      */
     @GetMapping("/addCompanyBusiness")
-    public Integer addCompanyBusinessByTycCompanyId(String companyId, String userName, String queryKeyword, String enterpriseType){
+    public Integer addCompanyBusinessByTycCompanyId(
+            @RequestParam("companyId") String companyId, @RequestParam("userName") String userName,
+            @RequestParam("queryKeyword") String queryKeyword, @RequestParam("enterpriseType") String enterpriseType
+    ) {
         return iPersonaCompanyBusinessService.addCompanyBusinessByTycCompanyId(companyId, userName, queryKeyword, enterpriseType);
     }
 
